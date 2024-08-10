6b7462bba1b54da743a878e33c4ec03d49b7044e
The-XENO-Studios <thexenostudios@gmail.com>
2024-08-10 11:29:29 +0600

    Tweak

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Footer />
-
           <ToastContainer bodyClassName={"Inter"} />
         </ThemeProvider>
       </body>
