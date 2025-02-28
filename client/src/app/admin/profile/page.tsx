ae15e730c9d648d652ce9aa046316fde71298b5c
t4sn33m-s4h4t <mdsahat6397@gmail.com>
2025-03-01 02:27:26 +0600

    added admin profile page"

--- client/src/app/admin/profile/page.tsx
+++ client/src/app/admin/profile/page.tsx
@@ -2,6 +2,9 @@ import AdminProfileCard from '@/components/Admin/Profile/AdminProfileCard'
 import { Spotlight } from '@/components/ui/Spotlight/Spotlight'
 import React from 'react'
 import ExtendedColors from '../../../../color.config'
+import ParticipantsInfoCard from '@/components/Admin/Profile/ParticipantsInfoCard'
+import NoticeCard from '@/components/Admin/Profile/NoticeCard'
+import MessageCard from '@/components/Admin/Profile/MessageCard'
 
 const page = () => {
     return (
@@ -12,6 +15,11 @@ const page = () => {
             />
             <section className="container mt-80 mb-32 flex flex-col gap-6 antialiased min-h-screen">
                 <AdminProfileCard />
+                <ParticipantsInfoCard />
+                <div className='grid grid-cols-1 gap-5 md:grid-cols-2'>
+                    <NoticeCard />
+                    <MessageCard />
+                </div>
             </section>
         </main>
     )
--- client/src/components/Admin/Profile/AdminProfileCard.tsx
+++ client/src/components/Admin/Profile/AdminProfileCard.tsx
@@ -3,23 +3,23 @@ import { FaUserEdit } from "react-icons/fa";
 
 const AdminProfileCard = () => {
   return (
-    <div className="py-10 px-16  bg-primary-550   rounded-3xl shadow-lg text-white">
-      <div className="flex items-center justify-between ">
+    <div className="py-10 md:px-16 px-5 bg-primary-550   rounded-3xl shadow-lg text-white">
+      <div className="flex flex-col lg:flex-row items-center justify-between  gap-10">
 
-        <div className="flex items-center space-x-4">
-          <div className=" -mt-96 rounded-full flex items-center bg-gradient-to-tr from-primary-500 to-primary-200 p-2 justify-center">
+        <div className="flex flex-col lg:flex-row items-center space-x-4">
+          <div className=" lg:-mt-96 rounded-full flex items-center bg-gradient-to-tr from-primary-500 to-primary-200 p-2 justify-center">
             <div className="w-full h-full rounded-full flex items-center justify-center ">
               <img src="https://aaastriping.ca/wp-content/uploads/2017/01/temp-image-300x224.jpg" alt="" className="rounded-full h-64 w-64 object-cover" />
             </div>
           </div>
-          <div className="space-y-5">
-            <p className="text-6xl font-bold">admin420</p>
-            <p className="text-2xl text-gray-400">admin420@gmail.com</p>
-            <button className="mt-2 px-10 py-4 text-xl bg-gradient-to-r from-primary-400 to-secondary-400 hover:from-primary-300 hover:to-secondary-300  font-medium rounded-full flex items-center space-x-2">
-              <span>Edit Profile</span>
+          <div className="space-y-5 text-center lg:text-left  lg:mt-0 mt-5">
+            <p className="lg:text-6xl font-bold text-3xl md:text-4xl">admin420</p>
+            <p className="text-xl md:text-2xl text-gray-400">admin420@gmail.com</p>
+            <button className="mt-2 mx-auto lg:mx-0 px-10 py-4 text-xl bg-gradient-to-r from-primary-400 to-secondary-400 hover:from-primary-300 hover:to-secondary-300  font-medium rounded-full flex items-center space-x-2">
+              <span >Edit Profile</span>
               <FaUserEdit />
             </button>
-            <p className="text-gray-400 text-xl cursor-pointer">Other Admins ▼</p>
+            <p className="text-gray-400 text-xl cursor-pointer ">Other Admins ▼</p>
           </div>
         </div>
 
