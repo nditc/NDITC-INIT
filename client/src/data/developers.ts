2a6e065b129654a074455636b90ac40ff3545caf
Mohammad Nafis Fuad <166198863+nafis2024@users.noreply.github.com>
2025-04-04 07:07:59 +0600

    Create developers.ts

---
+++ client/src/data/developers.ts
@@ -0,0 +1,16 @@
+import { Developer } from '../types/developer';
+
+export const developers: Developer[] = [
+  {
+    name: "Mohammad Nafis Fuad",
+    role: "Frontend Developer",
+    image: "",
+    facebook: "#"
+  },
+  {
+    name: "Dev Name",
+    role: "Dev Designation",
+    image: "Dev Image",
+    facebook: "#"
+  },
+];
