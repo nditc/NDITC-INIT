34d48fde45d43204a52e1d422a76d6333656ba76
RafsanAmin <rafsanamin2020@gmail.com>
2026-03-02 19:14:01 +0600

    security

--- server/config/rateLimiter.js
+++ server/config/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const createLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  max: 25, // limit each IP to 25 requests per windowMs
+  max: 35, // limit each IP to 25 requests per windowMs
   message: 'Too many create requests from this IP, please try again after a minute',
   standardHeaders: true,
   legacyHeaders: false,
@@ -10,7 +10,7 @@ const createLimiter = rateLimit({
 
 const updateLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  max: 25, // limit each IP to 25 requests per windowMs
+  max: 35, // limit each IP to 25 requests per windowMs
   message: 'Too many update requests from this IP, please try again after a minute',
   standardHeaders: true,
   legacyHeaders: false,
@@ -18,7 +18,7 @@ const updateLimiter = rateLimit({
 
 const getLimiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 1 minute
-  max: 2500, // limit each IP to 250 requests per windowMs
+  max: 3600, // limit each IP to 250 requests per windowMs
   message: 'Too many get requests from this IP, please try again after a minute',
   standardHeaders: true,
   legacyHeaders: false,
