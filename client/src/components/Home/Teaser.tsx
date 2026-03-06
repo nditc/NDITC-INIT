24822df598d7f3f9b2ddad8c6b0ffbe3bb259bd2
Arnab <arnabxarnab06@gmail.com>
2026-03-06 20:06:21 +0600

    Responsiveness issue resolved

--- client/src/components/Home/Teaser.tsx
+++ client/src/components/Home/Teaser.tsx
@@ -3,9 +3,21 @@ import React from "react";
 const Teaser = () => {
   return (
     <div className="mb-16 w-full">
-      <div className="container-c flex flex-col items-center justify-center gap-6">
+      <div className="container-c flex flex-col items-center gap-6">
         <h2 className="title title-top mb-0">VIDEO TEASER</h2>
-        <iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F1966138250778744%2F&show_text=false&width=560&t=0" width="560" height="314" style={{ border: "none", overflow: "hidden" }} scrolling="no" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+
+        <div className="w-full max-w-[560px]">
+          <div className="relative w-full pb-[56.25%]">
+            <iframe
+              src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F1966138250778744%2F&show_text=false&t=0"
+              className="absolute top-0 left-0 w-full h-full"
+              style={{ border: "none", overflow: "hidden" }}
+              scrolling="no"
+              frameBorder="0"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
       </div>
     </div>
   );
